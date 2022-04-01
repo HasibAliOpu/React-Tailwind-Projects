d1acaeffaa30c2d2d16edeb8d66d26865385d24a
H_A _Opu <wwwhasib4@gmail.com>
2022-04-02 02:45:24 +0600

    responsive navbar done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import CustomLink from "../../CustomLink/CustomLink";
 import Logo from "../../images/icon/company-logo.png";
+import { MenuIcon, XIcon } from "@heroicons/react/solid";
 const Header = () => {
+  const [open, setOpen] = useState(false);
   return (
     <div>
-      <div className="md:flex justify-between items-center p-4">
+      <div
+        onClick={() => setOpen(!open)}
+        className="w-8 text-sky-400  md:hidden"
+      >
+        {open ? <XIcon></XIcon> : <MenuIcon></MenuIcon>}
+      </div>
+      <div
+        className={`md:flex justify-between items-center p-4 md:static w-full duration-1000 ease-out bg-white absolute ${
+          open ? "static" : "top-[-320px]"
+        }`}
+      >
         <div className="flex justify-center mb-4 md:mb-0 md:static">
           <img src={Logo} className="w-20" alt="" />
         </div>
